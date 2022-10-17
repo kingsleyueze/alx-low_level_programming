5941506185219219cd4bcd4b2b381bb8cea7756d
Eze Kingsley Uzochukwu <kingsleyeu13@gmail.com>
2022-10-18 00:23:46 +0100

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+	char low;
+
+	for (digit = '0'; digit <= '9'; digit++)
+		putchar(digit);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
