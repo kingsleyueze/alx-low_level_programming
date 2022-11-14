982d818f5b742f8fd9b08e91890d3c33902392ea
Eze Kingsley Uzochukwu <kingsleyeu13@gmail.com>
2022-11-15 00:29:44 +0100

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for a basic dog.
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: dog's owner.
+ * Description: data structure for dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
